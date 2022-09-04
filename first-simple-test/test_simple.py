0b2a1cd9b5964104cb8d291bd1a3b5a78e0e3cca
omar_hasan <omar.hasan@udplatforms.com>
2022-09-04 15:35:46 +0600

    basic pytest

---
+++ first-simple-test/test_simple.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_file_method():
+    x = 5
+    y = 6
+    assert x+1 == y, "test failed"
+    assert x == y, "test failed"
+
+def test_file_method2():
+    x = 5
+    y = 6
+    assert x + 1 == y, "test failed"
